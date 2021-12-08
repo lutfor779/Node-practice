cf90274e17bb61c4a6ca079b7cbb50e9769505b6
lutfor779 <mdlrrahman779@gmail.com>
2021-12-08 13:33:43 +0600

    forgot to git

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 const AddUser = () => {
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const handleAddUser = e => {
+        e.preventDefault();
+        const name = nameRef.current.value;
+        const email = emailRef.current.value;
+
+        const newUser = { name, email };
+        fetch('http://localhost:5000/users', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newUser)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId) {
+                    alert('Successfully added the user.');
+                    e.target.reset();
+            }
+        })
+    }
+
     return (
         <div>
-            <h2>This is Add User</h2>
+            <h2>Please, This is Add User</h2>
+            <form onSubmit={ handleAddUser}>
+                <input type="text" ref={nameRef} />
+                <input type="email" ref={emailRef} />
+                <input type="submit" value="Add" />
+            </form>
         </div>
     );
 };
